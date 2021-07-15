71ff531dcd6217af3d2a95c9ba9ec8fba879d454
Nolan Woods <nolan_w@sfu.ca>
2021-07-15 16:52:46 -0700

    Add tag variable to k8s deployment

--- destinations/k8s/main.tf
+++ destinations/k8s/main.tf
@@ -3,5 +3,8 @@ module "cvmfs" {
   cvmfs_keys = {
     "microbedb.brinkmanlab.ca" = file(var.key_path == "" ? "${abspath(path.module)}/../../microbedb.brinkmanlab.ca.pub" : var.key_path)
   }
+  cvmfs_repo_tags = var.tag == null ? null : {
+    "microbedb.brinkmanlab.ca" = var.tag
+  }
   servers = ["http://stratum-1.sfu.brinkmanlab.ca/cvmfs/@fqrn@", "http://stratum-1.cedar.brinkmanlab.ca/cvmfs/@fqrn@"]
 }
--- destinations/k8s/variables.tf
+++ destinations/k8s/variables.tf
@@ -3,3 +3,9 @@ variable "key_path" {
   default = ""
   description = "Path on host to microbedb CVMFS repository pub key"
 }
+
+variable "tag" {
+  type = string
+  default = null
+  description = "CVMFS commit tag to mount"
+}
